6af540ed03b40248e55e616cf57e207527de781d
baby0 <1277704990@qq.com>
2018-01-23 14:03:13 +0800

    first commit

---
+++ src/main/java/com/bw/lenovo/xxxx/Xxxx.java
@@ -0,0 +1,15 @@
+package com.bw.lenovo.xxxx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Xxxx extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xxxx);
+
+
+    }
+}
